4d9048661dba6ce3c22320bef254fc466f4a634f
Alex Nielsen <alex@bigal.org>
2024-03-11 09:44:09 -0400

    finished 9.23

--- src/components/SinglePatient/index.tsx
+++ src/components/SinglePatient/index.tsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { apiBaseUrl } from '../../constants';
 import axios from 'axios';
 import patientService from "../../services/patients";
-import { Patient, Gender } from '../../types';
+import { Patient, Gender, Entry } from '../../types';
 
 function SinglePatient() {
     const location = useLocation();
@@ -38,10 +38,23 @@ function SinglePatient() {
     return (
         <div>
             <h1>SinglePatient</h1>
-            <p>Name: {patient.name} - ID:{patient.id}</p>
-            <p>DOB: {patient.dateOfBirth} - Gender: {patient.gender}</p>
-            <p>Occupation: {patient.occupation} - SSN: {patient.ssn}</p>
-            <p>Entries: </p>
+            <h2>Name: {patient.name} - Gender: {patient.gender}</h2>
+            <p>ID:{patient.id}</p>
+            <p>DOB: {patient.dateOfBirth} - SSN: {patient.ssn}</p>
+            <p>Occupation: {patient.occupation}</p>
+            <h3>Entries:
+            </h3>
+            {patient.entries.map((entry: Entry) => {
+                return (<div key={entry.id}>
+
+                    <p>{entry.date} - {entry.description}</p>
+                    <ul>
+                        {entry.diagnosisCodes?.map(code => <li key={code}>{code}</li>)}
+
+                    </ul>
+                </div>
+                );
+            })}
         </div>
     );
 }
